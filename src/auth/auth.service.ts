b67a5864213db427d7b61ced87c25ca2c7a12806
Cattyman <88530324+cattyman919@users.noreply.github.com>
2023-08-23 17:40:28 +0700

    Added Encryption to passwords

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,34 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+
 import { registerDto } from './dto/register.dto';
 import { UsersService } from 'src/users/users.service';
 import { LoginDTO } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UsersService) {}
   async register(registerData: registerDto) {
-    return await this.userService.createUser(registerData);
+    const hashedPassword = await bcrypt.hash(registerData.password, 10);
+    return await this.userService.createUser({
+      ...registerData,
+      password: hashedPassword,
+    });
   }
   async login(loginData: LoginDTO) {
     const user = await this.userService.findByEmail(loginData.email);
     if (!user)
-      throw new HttpException('User Does Not Exist', HttpStatus.NOT_FOUND);
+      throw new HttpException('Invalid Credentials', HttpStatus.BAD_REQUEST);
+    const validCredentials = await this.verifyPassword(
+      loginData.password,
+      user.password,
+    );
+    if (!validCredentials)
+      throw new HttpException('Invalid Credentials', HttpStatus.BAD_REQUEST);
     return user;
+  }
+
+  async verifyPassword(plainText, hashedText) {
+    return bcrypt.compare(plainText, hashedText);
   }
 }
