dd48ab3096a572be1f233b467cfd77a840ee5b04
Cattyman <88530324+cattyman919@users.noreply.github.com>
2023-08-25 16:15:56 +0700

    Fix unecessary code

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,9 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
-import { TaskStatus } from 'tasks/enums/task.enums';
-import { IsEnum } from 'class-validator';
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {
-  @IsEnum(TaskStatus)
-  status: TaskStatus;
-}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
